bb860719c53b555476d42dc3694ec5875350fa93
0199kye <70757090+0199kye@users.noreply.github.com>
2020-11-12 15:35:30 +0900

    submission3

--- main.c
+++ main.c
@@ -6,15 +6,14 @@
 int main(int argc, char *argv[]) {
 	
 	int i = 10;
-	int *ptr = &i;
+	int *p= &i;
+	int **q = &p;
 	
-	printf("i=%d, pi: %p\n", i, ptr);
-	(*ptr)++;
-
-    printf("i=%d, pi: %p\n", i, ptr);
-	*ptr++;
+	*p = 200;
+	printf("i = %d. *p= %d, **q = %d\n", i, *p, **q);
 	
-	printf("i=%d, pi: %p\n", i, ptr);
-		
+	**q=300;
+	printf("i = %d. *p= %d, **q = %d\n", i, *p, **q);
+	
 	return 0;
 }
